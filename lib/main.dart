99f25c34133bd5977519b5e36009e6220061b0de
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-08-29 14:26:45 +0430

    Added constants.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(CalculatorNewMe());
 }
 
+class CalculatorNewMe extends StatefulWidget {
+  const CalculatorNewMe({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatorNewMe> createState() => _CalculatorNewMeState();
+}
+
+class _CalculatorNewMeState extends State<CalculatorNewMe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
