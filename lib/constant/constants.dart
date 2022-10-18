69b6b6275171cbe09beb5dd266ac44225b9ab78b
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-10-18 15:27:13 +0330

    1--add color

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -13,6 +13,7 @@ final Color bgPageTop = HexColor('#4B4F50');
 final Color bgPageBottom = HexColor('#25282D');
 final Color coTextDisplay = HexColor('#73746E');
 final Color coTextResult = HexColor('#A6AFAE');
+// final Color coTextResult = HexColor('#A6AFAE');
 
 
 
